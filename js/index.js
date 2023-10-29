0b89ed606ba8170f26d307ae61519dfef10a3f13
florencialarreategui <florencialarreategui@gmail.com>
2023-10-29 02:25:12 -0300

    enlazo todos lo juegos y quedan funcionando

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 import { ahorcadoInicio } from "./ahorcado.js"
 import {ruletaInicio} from "./ruleta.js"
-// import {iniciarJuego} from "./preguntados.js"
+import {iniciarPreguntados} from "./preguntados.js"
  
 const botonAhorcado = document.getElementById("btn-play") 
 const botonRuleta = document.getElementById("boton-tirar") 
-
+const botonPreguntados =document.getElementById("start-preguntados")
 botonAhorcado.addEventListener("click", () => {
     ahorcadoInicio()
 });
 botonRuleta.addEventListener("click", () => {
     ruletaInicio()
 });
+
+botonPreguntados.addEventListener("click", () =>{
+    iniciarPreguntados()
+})
